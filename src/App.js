b75e3247cdc2590f37c1d74ffdbf12425cf742db
Shubham Porte <shubhamporte0@gmail.com>
2023-05-06 03:19:34 +0530

    My first Git commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { AppContext } from "./context/contextApi";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import SearchResult from "./components/SearchResult";
+import Feed from "./components/Feed";
+import SearchResultVideoCard from "./components/SearchResultVideoCard";
+import LeftNav from "./components/LeftNav";
+import LeftNavMenuItem from "./components/LeftNavMenuItem";
+import SuggestionVideoCard from "./components/SuggestionVideoCard";
+import VideoCard from "./components/VideoCard";
+import VideoDetails from "./components/VideoDetails";
+
+function App() {
+  return (   
+      <AppContext>
+        <BrowserRouter>
+          <div className="flex flex-col h-full">
+            <Header />
+            <Routes>
+              <Route path="/" exact element={<Feed />} />
+              <Route path="/searchResult/:searchQuery" element={<SearchResult />} />
+              <Route path="/video/:id" element={<VideoDetails />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AppContext>
+  );
+}
+
+export default App;
---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key": '1b77c8c95bmsh5c795b575a59cc3p12d33cjsn554cdb2bd65f',
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
